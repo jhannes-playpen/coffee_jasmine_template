c9b4ed973b5ee77af4491a1b0c0c43577cce1503
Ivar Nilsen <ivar.nilsen@gmail.com>
2011-10-19 21:41:11 +0200

    Removed js files

--- app/src/example.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  exports.Example = (function() {
-    function Example() {}
-    Example.prototype.sayHello = function(person) {
-      return "Hello " + person;
-    };
-    return Example;
-  })();
-}).call(this);
